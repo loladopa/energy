b2e79e535bb6f53f31b1633bac948ee253ec8a67
loladopa <loladopa@mail.ru>
2025-01-19 23:01:13 +0300

    en_ 8_2

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ mongoose.connect('mongodb://localhost/enegry')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var energy = require('./routes/energy');
 
 var app = express();
 
@@ -24,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/energy', energy);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));

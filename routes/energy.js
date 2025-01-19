2c5c62cbc7b4982851f8d35043c5c4401080471e
loladopa <loladopa@mail.ru>
2025-01-19 23:19:30 +0300

    en_ 8_3

--- routes/energy.js
+++ routes/energy.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var Energy = require('../models/energy').Energy;
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,4 +12,16 @@ router.get("/:nick", function(req, res, next) {
     res.send(req.params.nick);
     });
     
+    /* Страница 'енергетиков' */
+router.get("/:nick", async function(req, res, next) {
+    var energys = await Energy.find({nick: req.params.nick});
+    console.log(energys)
+    if(!energys.length) return next(new Error("Нет такого энергетика на моем сайте"))
+    var energy = energys[0];
+    res.render('energy', {
+        title: energy.title,
+        picture: energy.avatar,
+        desc: energy.desc
+    })
+});
 module.exports = router;

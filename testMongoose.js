0bc35fe918953f2c0fcc9c5158ce444f6936e01e
loladopa <loladopa@mail.ru>
2025-01-19 21:54:55 +0300

    en_ 7_1

---
+++ testMongoose.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
+const Cat = mongoose.model('Cat', { name: String });
+const kitty = new Cat({ name: 'жестяной' });
+kitty.save().then(() => console.log('енергитики'));

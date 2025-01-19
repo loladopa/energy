7482eceea5030e19d7d0b32a1949738009df4587
loladopa <loladopa@mail.ru>
2025-01-19 19:42:33 +0300

    en_ 4_2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
+  });
+/* Страница Burn*/
+router.get('/burn', function(req, res, next) {
+  res.send("<h1>Страница Burn</h1>")
+  });
+/* Страница Tornado */
+router.get('/tornado', function(req, res, next) {
+  res.send("<h1>Страница Tornado</h1>")
+  });
+  /* Страница Gorilla */
+  router.get('/gorilla', function(req, res, next) {
+  res.send("<h1>Страница Gorilla</h1>")
+  });
+  
 module.exports = router;
